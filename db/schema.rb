55ad48f4d4becbd057cb4e957582c11dfc331c0b
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-02-21 01:53:02 -0300

    devise model done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210221040044) do
+ActiveRecord::Schema.define(version: 20210221045104) do
 
   create_table "stories", force: :cascade do |t|
     t.string "title"
@@ -21,12 +21,17 @@ ActiveRecord::Schema.define(version: 20210221040044) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "name"
-    t.string "email"
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
